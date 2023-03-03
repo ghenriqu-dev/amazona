a4c86050a3b0fadb48f69fff869909256f06e43a
ghenriqu-dev <ghenriqu.dev@gmail.com>
2023-03-03 02:59:58 -0300

    first commit

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <div>
+      <header>
+        <a href="/">amazona</a>
+      </header>
+      <main>List Products</main>
+      <h1>Churiço</h1>
+      <h1>Churiço 20</h1>
+    </div>
+  );
+}
+
+export default App;
